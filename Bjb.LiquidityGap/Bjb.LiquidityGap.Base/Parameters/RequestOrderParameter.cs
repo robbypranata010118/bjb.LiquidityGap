ddf9e630094d6da7aa75212a81ab0631efdee3cd
Robby Pranata <70742769+robbypranata010118@users.noreply.github.com>
2022-02-05 02:57:21 +0700

    Revert "Feature/add base entity"

--- Bjb.LiquidityGap/Bjb.LiquidityGap.Base/Parameters/RequestOrderParameter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bjb.LiquidityGap.Base.Parameters
-{
-    public class RequestOrderParameter
-    {
-        public string Field { get; set; }
-        public string Order { get; set; }
-    }
-}
