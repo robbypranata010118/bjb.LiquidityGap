75de3c6f465112f993905ef4d60f256169e6cc3d
Asep Saepul Rohmat <asepsaepulr88@gmail.com>
2022-02-07 22:56:06 +0700

    Fix Data Null SubCategory & DataSource on List Pagination

--- Bjb.LiquidityGap/Bjb.LiquidityGap.WebApi/Examples/SheetItem/GetPaginationExample.cs
+++ Bjb.LiquidityGap/Bjb.LiquidityGap.WebApi/Examples/SheetItem/GetPaginationExample.cs
@@ -1,6 +1,30 @@
-﻿namespace Bjb.LiquidityGap.WebApi.Examples.SheetItem
+﻿using Bjb.LiquidityGap.Application.Features.SheetItems.Queries.Get;
+using Bjb.LiquidityGap.Base.Parameters;
+using Swashbuckle.AspNetCore.Filters;
+using System.Collections.Generic;
+
+namespace Bjb.LiquidityGap.WebApi.Examples.SheetItem
 {
-    public class GetPaginationExample
+    public class GetPaginationExample : IExamplesProvider<GetSheetItemQuery>
     {
+        public GetSheetItemQuery GetExamples()
+        {
+            List<RequestFilterParameter> filterParameters = new List<RequestFilterParameter>();
+            filterParameters.Add(new RequestFilterParameter
+            {
+                Field = "Name",
+                ComparisonOperator = "like",
+                Type = "string",
+                Value = "Pos Akun 1"
+            });
+            return new GetSheetItemQuery
+            {
+                Page = 1,
+                Length = 10,
+                Filters = filterParameters,
+                Orders = new List<string> { "Name" },
+                SortType = "ASC"
+            };
+        }
     }
 }
--- Bjb.LiquidityGap/Bjb.LiquidityGap.WebApi/Examples/SheetItem/UpdateDataSourceExample.cs
+++ Bjb.LiquidityGap/Bjb.LiquidityGap.WebApi/Examples/SheetItem/UpdateDataSourceExample.cs
@@ -1,6 +1,25 @@
-﻿namespace Bjb.LiquidityGap.WebApi.Examples.SheetItem
+﻿using Bjb.LiquidityGap.Application.Features.SheetItems.Commands.Update;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Bjb.LiquidityGap.WebApi.Examples.SheetItem
 {
-    public class UpdateDataSourceExample
+    public class UpdateDataSourceExample : IExamplesProvider<UpdateSheetItemCommand>
     {
+        public UpdateSheetItemCommand GetExamples()
+        {
+            return new UpdateSheetItemCommand()
+            {
+                Id = 1,
+                SubCategoryId = 1,
+                DataSourceId = 1,
+                Code = "111000",
+                Name = "Pos Akun 1",
+                SheetItemParentId = null,
+                MarkToCalculate = true,
+                Statement = "ya",
+                IsManualInput = true
+
+            };
+        }
     }
 }
